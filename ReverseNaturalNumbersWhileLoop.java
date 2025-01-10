dc2efcd99824d20d8386616b8cda539ca4688201
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2025-01-10 23:13:21 +0530

    Create ReverseNaturalNumbersWhileLoop.java

---
+++ ReverseNaturalNumbersWhileLoop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReverseNaturalNumbersWhileLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
+
+        System.out.println("The first " + n + " natural numbers in reverse order are:");
+        while (n >= 1) {
+            System.out.print(n + " ");
+            n--;
+        }
+
+        scanner.close();
+    }
+}
