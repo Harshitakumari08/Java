575d3e0247e6ccd6901a021fd1cc32b3ee2e4f6f
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2025-01-10 23:10:09 +0530

    Create DecrementingForLoop.java

---
+++ DecrementingForLoop.java
@@ -0,0 +1,10 @@
+public class DecrementingLoop {
+    public static void main (String[] args){
+        int i;
+        for(i=7; i!=0; i--){
+            System.out.println(i);
+
+        }
+    
+    }
+}
