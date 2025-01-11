e590e64fa7d97506f5abb30e21f97a93f67349db
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2025-01-11 23:21:09 +0530

    Create KmToMiles.java

---
+++ KmToMiles.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class KmToMiles {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the length in kilometer: ");
+        float kilometer = scanner.nextFloat();
+        
+        
+        float miles= kilometer * 0.621371f;
+        System.out.println("Here's the length in miles will be: " + miles);
+
+        scanner.close();
+    }
+}
