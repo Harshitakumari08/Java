b81ed0ed66a9f4f15a808bc723763026474e4d89
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2025-01-11 23:47:18 +0530

    Create FindingOutput.java

---
+++ FindingOutput.java
@@ -0,0 +1,9 @@
+public class FindingOutput {
+    public static void main(String[] args){
+        int y = 7;
+        int x = ++y * 8;
+        System.out.println("the value of x will be: " + x);
+    }
+}
+
+//output-the value of x will be: 64
